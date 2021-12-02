7fe008259a62c3bd6f0ce82fe0d276a3383afc2f
serdam <serdam@ubuntu>
2021-12-02 17:41:34 +0100

    ahora hay un bucle lol

--- Temperatura/src/temperatura/Temperatura.java
+++ Temperatura/src/temperatura/Temperatura.java
@@ -3,16 +3,20 @@ package temperatura;
 import java.util.Scanner;
 public class Temperatura {
     public static void main(String[] args) {
+        int temp;
+        int i = 0;
         System.out.println("Bienvenido");
-        System.out.println("Porfavor introduzca una temperatura.");
         Scanner entrada= new Scanner(System.in);
-        int temp= entrada.nextInt();
+        for (i= 1; i<=5; i++) {
+        System.out.println("Porfavor introduzca una temperatura.");
+        temp= entrada.nextInt();
         if (temp<0) {
             System.out.println("Es negativa.");
         } else if (temp>0) {
             System.out.println("Es positiva.");
         } else {
             System.out.println("Es 0.");
+        }
         }
     }
     
