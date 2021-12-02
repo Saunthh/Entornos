457f2adf3458c4ea62981af1864dce10d948b888
serdam <serdam@ubuntu>
2021-12-02 18:54:52 +0100

    He mejorados las variebles mayor y menor.

--- Temperatura/src/temperatura/Temperatura.java
+++ Temperatura/src/temperatura/Temperatura.java
@@ -1,35 +1,42 @@
-
 package temperatura;
+
 import java.util.Scanner;
+
 public class Temperatura {
+
     public static void main(String[] args) {
-        Scanner entrada= new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
         double temp;
         int n;
-        double menor=0;
-        double mayor=0;
-        
+        double menor = 0;
+        double mayor = 0;
+
         System.out.println("Bienvenido escriba cuantos municipios va a poner.");
-        n= entrada.nextInt();
-        
-        for (int i= 1; i<=n; i++) {
-        System.out.println("Porfavor introduzca una temperatura.");
-        temp= entrada.nextInt();
-        if (temp<0) {
-            System.out.println("Es negativa.");
-        } else if (temp>0) {
-            System.out.println("Es positiva.");
-        } else {
-            System.out.println("Es 0.");
-            
+        n = entrada.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Porfavor introduzca una temperatura.");
+            temp = entrada.nextDouble();
+            if (temp < 0) {
+                System.out.println("Es negativa.");
+            } else if (temp > 0) {
+                System.out.println("Es positiva.");
+            } else {
+                System.out.println("Es 0.");
+
+            }
+            if (i == 1) {
+                mayor = temp;
+                menor = temp;
+            } else {
+                if (temp > mayor) {
+                    mayor = temp;
+                } else if (temp < menor) {
+                    menor = temp;
+                }
+            }
         }
-        if (temp>mayor){
-            mayor= temp;
-        } else if (temp<menor){
-            menor= temp;
-        }
-        }
-        System.out.println("La temperatura maxima obtenida es "+mayor+" y la temperatura menor obtenida es "+menor+".");
+        System.out.println("La temperatura maxima obtenida es " + mayor + " y la temperatura menor obtenida es " + menor + ".");
     }
-    
+
 }
