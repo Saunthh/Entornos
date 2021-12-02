7d9d53607a7cf59d3084477275f446a2a45bf9da
serdam <serdam@ubuntu>
2021-12-02 17:32:03 +0100

    He puesto mas cosas

--- Temperatura/src/temperatura/Temperatura.java
+++ Temperatura/src/temperatura/Temperatura.java
@@ -1,9 +1,19 @@
 
 package temperatura;
-
+import java.util.Scanner;
 public class Temperatura {
     public static void main(String[] args) {
         System.out.println("Bienvenido");
+        System.out.println("Porfavor introduzca una temperatura.");
+        Scanner entrada= new Scanner(System.in);
+        int temp= entrada.nextInt();
+        if (temp<0) {
+            System.out.println("Es negativa.");
+        } else if (temp>0) {
+            System.out.println("Es positiva.");
+        } else {
+            System.out.println("Es 0.");
+        }
     }
     
 }
