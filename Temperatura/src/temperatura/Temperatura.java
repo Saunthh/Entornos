3b6e6e56c17d259cefe984f8e6c69a887be1727f
serdam <serdam@ubuntu>
2021-12-02 17:43:06 +0100

    ahora hay un bucle lol y inicio la i como le guasta a aitor

--- Temperatura/src/temperatura/Temperatura.java
+++ Temperatura/src/temperatura/Temperatura.java
@@ -4,10 +4,9 @@ import java.util.Scanner;
 public class Temperatura {
     public static void main(String[] args) {
         int temp;
-        int i = 0;
         System.out.println("Bienvenido");
         Scanner entrada= new Scanner(System.in);
-        for (i= 1; i<=5; i++) {
+        for (int i= 1; i<=5; i++) {
         System.out.println("Porfavor introduzca una temperatura.");
         temp= entrada.nextInt();
         if (temp<0) {
